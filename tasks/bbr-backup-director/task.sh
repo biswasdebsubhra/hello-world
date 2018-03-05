ae3fba6966afa9f39a359048fd93a482cb79fd73
root <anil007kiran@gmail.com>
2018-03-05 15:41:17 +0000

    First commit

---
+++ tasks/bbr-backup-director/task.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -eu
+set -x 
+. "$(dirname $0)"/../../scripts/export-director-metadata
+echo First step is done
+
+om_cmd curl -p /api/v0/deployed/director/credentials/bbr_ssh_credentials > bbr_keys.json
+BOSH_PRIVATE_KEY=$(jq -r '.credential.value.private_key_pem' bbr_keys.json)
+
+pushd director-backup-artifact
+  ../binary/bbr director --host "${BOSH_ADDRESS}" \
+  --username bbr \
+  --private-key-path <(echo "${BBR_PRIVATE_KEY}") \
+  backup
+
+  tar -cvf director-backup.tar -- *
+popd
